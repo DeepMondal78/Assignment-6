eb7c1370b1ac28a2f6efd76733c29d0f60df6a6a
Deep Mondal <deepmondalkumar6@gmail.com>
2026-02-20 17:00:33 +0530

    Assignment 6 Add

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Assignment-6
+# SimpleWebServerwith-Node.js =====
+
+** This project is a simple Node.js server created using the built-in 'http' module. 
+It serves different HTML pages (Home, About, Contact) based on the route 
+and shows a 404 page for invalid routes....!   **
+
+project-folder/
+│-- server.js          
+│-- pages/
+│    ├── home.html
+│    ├── about.html
+│    ├── contact.html
+│    └── 404.html
+│-- README.md
+
+1. Open terminal and navigate to the project folder.
+2. Run the command: npm start
+3. Open browser and go to:
+   - Home: http://localhost:3000/
+   - About: http://localhost:3000/about
+   - Contact: http://localhost:3000/contact
+   - Invalid route: http://localhost:3000/xyz → shows 404 page (error pages)
+
+## screenshots
+### server Page
+![serverLog](./screenshots/server.png)
+
+### Home Page
+![Home Page](./screenshots/home.png)
+
+### About Page
+![About Page](screenshots/about.png)
+
+### Contact Page
+![Contact Page](./screenshots/contact.png)
+
+### 404 Page
+![404 Page](./screenshots/404.png)
---
+++ server.js
@@ -0,0 +1,46 @@
+// import http...
+let http = require('http');
+// import path here...
+let path = require("path");
+// import fs here...
+let fs = require("fs");
+
+// create server logic here...
+let app = http.createServer((req, res) => {
+    // path name variable here..
+    let pathName;
+    // destructor  here logic here  { url, methoed ,headers} = req 
+    const { url } = req;
+    if (url === '/' || url.toLowerCase() === '/home') {
+        pathName = path.join(__dirname, "pages", "home.html");
+    }
+    else if (url.toLowerCase() === "/about") {
+        pathName = path.join(__dirname, "pages", "about.html");
+    }
+    else if (url.toLowerCase() === "/contact") {
+        pathName = path.join(__dirname, "pages", "contact.html");
+    }
+    else {
+        pathName = path.join(__dirname, "pages", "404.html");
+    }
+    // file stystem  login here.....
+    fs.readFile(pathName, (error, data) => {
+        if (error) {
+            res.writeHead(400, { "Content-type": "text/pain" })
+            res.end("internal server error plz try agin leater")
+        }
+        else {
+            res.writeHead(200, { "Content-type": "text/html" })
+            res.end(data)
+        }
+    })
+})
+
+// port name here...
+let Port = 3000;
+
+// run server logic  here....
+app.listen(Port, () => {
+    console.log(`your node.js server is running http://localhost:${Port}`);
+})
+
